593e5a8371251f051f3be7d051bd8625f19a55d3
Erik Hoversten <ehoversten@gmail.com>
2015-12-13 12:23:44 -0500

    Initial Commit

---
+++ ProjectCapture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectCapture
+//
+//  Created by Erik Hoversten on 13.12.15.
+//  Copyright © 2015 EverProductions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProjectCapture/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ProjectCapture
+//
+//  Created by Erik Hoversten on 13.12.15.
+//  Copyright © 2015 EverProductions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
